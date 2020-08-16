4e02e21218969c501460e68d02437d45f8fd0b64
ceschi <emanuele.franceschi@gmail.com>
2020-08-16 15:08:10 +0200

    bars graphs titles

--- functs.R
+++ functs.R
@@ -702,7 +702,7 @@ plot_chunkregs_bar <- function(chunk_regs_obj, graphs_dir. = graphs_dir, name){
     unique() %>% 
     paste0('sum of AR(', ., ') coefficients')
   
-  tt <- paste0(name,
+  tt <- paste0(name %>% noms_tt(),
                ': ', len, ' chunks with forecasts - ',
                labely)
   
