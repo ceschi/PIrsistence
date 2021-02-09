e09ffd108200dab6adfc0d27764d75bc640d186e
ceschi <emanuele.franceschi@gmail.com>
2021-02-09 21:37:26 +0100

    draft for HL

--- newfunct.R
+++ newfunct.R
@@ -45,7 +45,7 @@ k_fullsample_1l <- function(data,
   if (batch_prime>.25*n_sample){
     
     # take second last prime factor
-    batch_prime <- tail(prime_fs, 2) %>% .[1]
+    batch_prime <- tail(prime_fs, 2) %>% head(1)
   }
       
     
@@ -63,9 +63,7 @@ k_fullsample_1l <- function(data,
       # internally determine validation data,
       # by construction sizes are consistent and evenly divide
       # train and validation.
-      
-      #' *needs data_lagged first!*
-      
+
       # n_sample to account val
       n_train <- n_sample - batch_prime
       
@@ -90,7 +88,7 @@ k_fullsample_1l <- function(data,
                          dim = c(batch_prime, n_steps, n_feat))
     }
 
-  }else if (!is.null(data_val)){
+  }else if (!is.null(data_val)){ # usr provides data: lagged or not?
     if (nrow(data_val)<=n_steps) stop('\n{data_val} is too short wtr lags {n_steps}, adjust either of them.\n')
     
     # user provides val data, need to check
@@ -100,15 +98,20 @@ k_fullsample_1l <- function(data,
     
     data_val_n_sample <- nrow(data_val) - n_steps
     
-    # check consistency with val&train
-    if (usr_batch){
+    # check consistency with val & train
+    #' when usr provides *val & batch*
+    if (usr_batch){ # usr provided batch size, check compat, lag data
       
-      # test
+      # test on validation
       data_val_gate <- data_val_n_sample%%size_batch == 0
       if (!data_val_gate) stop('\nProvided incompatible {size_batch} or {data_val}, must evenly divide sample sizes less lags {n_steps}.\n')
       
+      # test on train
       data_train_gate <- n_sample%%size_batch == 0
       if (!data_train_gate) stop('\nProvided incompatible {size_batch} or {data}, must evenly divide sample sizes less lags {n_steps}.\n')
+      
+      
+      
     }else if (size_batch == 'auto'){
       
       # test: even division with val data
@@ -120,6 +123,8 @@ k_fullsample_1l <- function(data,
       if (length(primes_cand)==0) stop('\nNo common factor in {data} and {data_val}: vary {n_steps} or activate {internal_validation = T}\n')
       
       if (!(batch_prime %in% primes_cand)) stop('\nNo common factor in {data} and {data_val}: vary {n_steps} or activate {internal_validation = T}\n')
+      
+      
       
     }
     
@@ -151,3 +156,113 @@ k_fullsample_1l <- function(data,
 #     full run of batcher
 #   else if batch == 1
 #     do nothing, carry on
+
+
+#TODO: HALFLIFE SHOCK DECAY ####################################################
+
+#TODO: HALFLIFE FUNCTION WITH ESTIMATED MODEL
+
+ysin <- tibble(y = 2*sin(2*base::pi*1:300))
+
+yy <- data_prepper(ysin, train = .8, .2)
+
+
+steps <- 15
+feat <- 1
+batch <- 5
+
+
+yy_lag_t <- embed(yy$train$train_norm, steps+1)
+yy_lag_v <- embed(yy$test$test_norm, steps+1)
+
+t_sample <- nrow(yy_lag_t)
+v_sample <- nrow(yy_lag_v)
+
+yy_arr_ty <- array(yy_lag_t[,1], dim = c(t_sample, feat))
+yy_arr_tx <- array(yy_lag_t[,-1], dim = c(t_sample, steps, feat))
+
+yy_arr_vy <- array(yy_lag_v[,1], dim = c(t_sample, feat))
+yy_arr_vx <- array(yy_lag_v[,-1], dim = c(t_sample, steps, feat))
+  
+mdl_tr <- keras_model_sequential() %>% 
+  layer_lstm(units = 20,
+             input_shape = c(steps, feat), 
+             return_sequences = F,
+             # batch_input_shape = c(5, 1),
+             stateful = T,
+             batch_size = batch
+             ) %>% 
+  layer_dense(units = 1) %>% 
+  compile(optimizer= 'adam', loss = 'mse')
+
+
+his <- fit(mdl_tr,
+           x = yy_arr_tx,
+           y = yy_arr_ty,
+           shuffle = F,
+           # validation_split = .5,
+           validation_data = list(yy_arr_vx, yy_arr_vy),
+           epochs = 1300,
+           batch_size = batch
+           )  
+
+
+foca <- rep(0, v_sample-1) %>% c(.,1) %>% array(dim = c(v_sample,1))
+
+
+
+mdl_tr$get_input_shape_at(0L) %>% sapply(c)
+
+
+mdl_1 <- keras_model_sequential() %>% 
+  layer_lstm(units = 20,
+             input_shape = c(steps, feat), 
+             return_sequences = F,
+             # batch_input_shape = c(5, 1),
+             stateful = T,
+             batch_size = 1
+  ) %>% 
+  layer_dense(units = 1) %>% 
+  compile(optimizer= 'adam', loss = 'mse')
+
+# this one works inplace
+set_weights(mdl_1, weights = get_weights(mdl_tr))
+
+input_lagged <- embed(foca, steps)
+
+in_arr <- array(input_lagged,
+                dim = c(nrow(input_lagged), ncol(input_lagged),1))
+
+sha <-  mdl_1$get_input_shape_at(0L) %>% sapply(c)
+
+llast <- nrow(in_arr)
+
+h <- 99
+# not sure it immediately converges to above .5
+# while (h>=.5){
+#   h <- predict(mdl_1,
+#                x = array(data = in_arr[llast, , ], dim = sha),
+#                batch_size = 1)
+# }
+
+for (i in 1:100){
+  
+  input_lagged <- embed(foca, steps)
+  
+  in_arr <- array(input_lagged,
+                  dim = c(nrow(input_lagged), ncol(input_lagged),1))
+  
+  llast <- nrow(in_arr)
+  
+  h <- predict(mdl_1,
+               x = array(data = in_arr[llast, , ], dim = sha),
+               batch_size = 1)
+  
+  foca <- c(foca, h)
+  
+  
+}
+
+ert <- bind_rows(tibble(yy_arr_vy) %>% add_column(lab = 'real'),
+                 predict(mdl_tr, x = yy_arr_vx[nrow(yy_arr_vx), , ], batch_size = 5) %>%tibble() %>%  add_column(lab = 'pred')
+                 )
